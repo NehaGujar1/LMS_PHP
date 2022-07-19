d2ed56da7127ca9ce7834d4d49b3ea0b596145a8
NehaGujar1 <98935998+NehaGujar1@users.noreply.github.com>
2022-07-19 20:55:52 +0530

    Update setup_shellscript.sh

--- setup_shellscript.sh
+++ setup_shellscript.sh
@@ -9,12 +9,9 @@ if [[ -f "config/config.php" ]]; then
 	php -S localhost:8001
 else
 	echo "We will have to create a new database inorder to import the schema sql"	
-	read -n 1 -r -s -p $'To proceed enter Y\n'
-	composer install
-	composer dump-autoload
 	$db_host,$db_port,$db_username,$db_name_of_database,$db_pwd
 	echo 'Enter the details for database config file'
-    read -p 'Enter the port for your database' db_port
+        read -p 'Enter the port for your database' db_port
 	read -p 'Enter the database host, it has no relation with "8001"' db_host
 	read -p 'Enter the name of the database to be used' db_name_of_database
 	read -p 'Enter the username if not enter "root"' db_username
@@ -27,7 +24,8 @@ else
 	echo '$DB_USERNAME= '$db_username';'>>config/config.php
 	echo '$DB_PASSWORD= '$db_pwd';'>>config/config.php
 	echo '?>'>>config/config.php
-	mysql -u $db_username -p $db_name<database.sql
+	echo “Now, we will import the schema”
+	mysql -u $db_username -p $db_name < schema.sql
 	echo "Starting server at port 8001"
 	cd public/
 	php -S localhost:8001
